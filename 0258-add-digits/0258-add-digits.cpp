7639fd600effdeb3119139636e1bdc90e767f5d9
Priyanshu Gupta <priyanshupg29@gmail.com>
2026-09-08 15:31:52 +0530

    Time: 0 ms (100%), Space: 8.7 MB (45.51%) - LeetHub

---
+++ 0258-add-digits/0258-add-digits.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int addDigits(int num) {
+        long sum=0;
+        while(num>0){
+            int digit=num%10;
+            sum+=digit;
+            num=num/10;
+            if(num==0 && sum>9) {
+                num=sum;
+                sum=0;
+            }
+
+        }
+        return sum;
+    }
+};
