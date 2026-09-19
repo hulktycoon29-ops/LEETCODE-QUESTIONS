283e542ae1311cc9f39338fc0006e1fee501dfb4
Priyanshu Gupta <priyanshupg29@gmail.com>
2026-09-19 23:38:13 +0530

    Time: 5 ms (25.21%), Space: 9.7 MB (48.32%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -6,11 +6,11 @@ public:
 
         map<char, int> mpp;
 
-        for (int i = 0; i < s.length(); i++) {
+        for (int i = 0; i < s.length(); i++){
             mpp[s[i]]++;
             mpp[t[i]]--;
         }
-
+        
         for (auto x : mpp) {
             if (x.second != 0)
                 return false;
